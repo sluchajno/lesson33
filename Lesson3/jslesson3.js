e1f2eeb93cb8c29d2882e4e8dda0ef677a34d707
sluchajno <72516983+sluchajno@users.noreply.github.com>
2020-10-14 23:49:15 +0300

    nooles3

---
+++ Lesson3/jslesson3.js
@@ -0,0 +1,35 @@
+// !!!! ЗАДАНИЕ 1 !!!!!
+// let prime = 1;
+// let primeArr = ;
+
+// while (prime <= 100) {
+//   
+// }
+
+
+
+
+// !!!! ЗАДАНИЕ 2 !!!!!
+
+
+function countBasketPrice() {
+
+}
+
+
+// !!!! ЗАДАНИЕ 3* !!!!!
+
+
+// for (let i = 0; i < 9; i++, console.log(i)) { }
+
+
+// !!!! ЗАДАНИЕ 4* !!!!!
+
+
+// let arr = [];
+// let end = 0;
+// while (end < 20) {
+//     end++;
+//     arr.push('x');
+//     console.log(arr);
+// }
